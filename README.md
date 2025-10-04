94495a0ef9a37929bd9f3d9f8b146f6a440827d7
Matthew Thorley <matthew.thorley@lvt.com>
2025-10-04 08:40:49 -0600

    Basic layout for one verse

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Matthew 5
 
 https://api.nlt.to/Manage
+
+
+React Native Pan Gesture
+
+https://www.youtube.com/watch?v=AVS_2nzt8Do
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,32 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 import bible from "../bible.json";
 console.log(bible.matthew["1"]);
 
 export default function Index() {
+  const [verses] = useState(bible.matthew["1"]);
   //const bible = bible["matthew"]["1"]["1"];
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>{bible.matthew["1"]}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Matthew 5:1</Text>
+      <Text style={styles.verse}>{bible.matthew["1"]}</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFBFF",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 60,
+    marginTop: 30,
+  },
+  verse: {
+    fontSize: 40,
+    margin: 30,
+  },
+});
